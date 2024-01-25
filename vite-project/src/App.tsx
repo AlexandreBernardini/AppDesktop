d7b310030ff3039e44e11fc3ac0e60ff3d94f4ec
Alexyo42 <120744066+Alexyo42@users.noreply.github.com>
2024-01-25 12:19:21 +0100

    ca marche

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,35 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState<User[]>([]);
+  const [selectedGender, setSelectedGender] = useState<string>('');
+  const [selectedAge, setSelectedAge] = useState<string>('');
+  const [search, setSearch] = useState<string>('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const filterByGender = (userList: User[]) => {
+    if (selectedGender === 'male' || selectedGender === 'female') {
+      return userList.filter(user => user.gender === selectedGender);
+    }
+    return userList;
+  };
+
+  const sortByAge = (userList: User[]) => {
+    if (selectedAge === 'asc') {
+      return [...userList].sort((a, b) => a.dob.age - b.dob.age);
+    }
+    if (selectedAge === 'desc') {
+      return [...userList].sort((a, b) => b.dob.age - a.dob.age);
+    }
+    return userList;
+  };
+
+  const searchUser = (userList: User[]) => {
+    if (search) {
+      return userList.filter(user => (
+        user.name.first.toLowerCase().includes(search.toLowerCase()) ||
+        user.name.last.toLowerCase().includes(search.toLowerCase())
+      ));
+    }
+    return userList;
+  };
+
+  const fetchUsers = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.get('https://randomuser.me/api/?results=20');
+      setUsers(response.data.results);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+    finally{
+      setIsLoading(false);
+    }
+  };
+
+  const usersToDisplay = searchUser(sortByAge(filterByGender(users)));
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     {isLoading && (
+        <div className="spinner-container">
+          <div className="loading-spinner">
+          </div>
+        </div>
+      )}
+      <div className={`content-container ${isLoading ? 'blur-background' : ''}`}>
+        <h1>Hello !</h1>
+        <hr />
+        <button onClick={fetchUsers} className="btn btn-primary" disabled={isLoading}>Fetch users</button>
+        <button onClick={() => setUsers([])} className="btn btn-danger" disabled={!users.length}>Clear users</button>
+        <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Rechercher un utilisateur" />
+        <hr />
+        <label htmlFor="selectedGender">Filtrer par :</label>
+        <select value={selectedGender} onChange={(e) => setSelectedGender(e.target.value)}>
+          <option value="male">homme</option>
+          <option value="female">femme</option>
+          <option value="both">les deux</option>
+        </select>
+        <hr />
+        <label htmlFor="selectedAge">Trier par :</label>
+        <select value={selectedAge} onChange={(e) => setSelectedAge(e.target.value)}>
+          <option value="asc">croissant</option>
+          <option value="desc">décroissant</option>
+          <option value="none">aucun</option>
+        </select>
+        <span id="user-length">{usersToDisplay.length}</span> utilisateurs
+        {usersToDisplay.length > 0 ? (
+          <table id="tbl-users" className="table table-hover">
+            <thead>
+              {usersToDisplay.map((user) => (
+                <tr key={user.email}>
+                  <td><img src={user.picture.thumbnail} alt="User Thumbnail" /></td>
+                  <td>{user.gender === 'male' ? 'Monsieur' : 'Madame'} {user.name.first} {user.name.last}</td>
+                  <td>{user.email}</td>
+                  <td>{user.phone}</td>
+                  <td>{user.dob.age}</td>
+                </tr>
+              ))}
+            </thead>
+          </table>
+        ) : (
+          <div className="alert alert-info">
+            <h1>pas d'utilisateurs</h1>
+          </div>
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
